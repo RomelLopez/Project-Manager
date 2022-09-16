412461cd3ccdc0755deae0f502c5df37a43bce7f
RomelLopez <romelanthonylopez@gmail.com>
2022-09-16 13:08:45 -0700

    finished project

--- client/src/mutations/projectMutations.js
+++ client/src/mutations/projectMutations.js
@@ -1,56 +1,66 @@
 import { gql } from '@apollo/client';
 
 const ADD_PROJECT = gql`
-mutation AddProject($name: String!, $description: String!, $status: ProjectStatus! $clientId: ID!) {
-    addProject(name: $name, description: $description, status: $status, clientId: $clientId) {
+  mutation AddProject(
+    $name: String!
+    $description: String!
+    $status: ProjectStatus!
+    $clientId: ID!
+  ) {
+    addProject(
+      name: $name
+      description: $description
+      status: $status
+      clientId: $clientId
+    ) {
+      id
+      name
+      description
+      status
+      client {
         id
         name
-        description
-        status
-        client {
-            id
-            name
-            email
-            phone
-        }
+        email
+        phone
+      }
     }
-}
+  }
 `;
 
 const DELETE_PROJECT = gql`
-mutation DeleteProject($id: ID!) {
+  mutation DeleteProject($id: ID!) {
     deleteProject(id: $id) {
-        id
+      id
     }
-}
+  }
 `;
 
 const UPDATE_PROJECT = gql`
-mutation UpdateProject($id: ID!
-     $name: String!
-      $description: String!
-       $status: ProjectStatusUpdate!
-    ){
-    UpdateProject( 
-        id: $id
-        name: $name
-        description: $description
-        status: $status
-          ) {
+  mutation UpdateProject(
+    $id: ID!
+    $name: String!
+    $description: String!
+    $status: ProjectStatusUpdate!
+  ) {
+    updateProject(
+      id: $id
+      name: $name
+      description: $description
+      status: $status
+    ) {
+      id
+      name
+      description
+      status
+      client {
         id
         name
-        description
-        status
-        client {
-            id
-            name
-            email
-            phone
-        }
+        email
+        phone
+      }
     }
-}
+  }
 `;
-
 
 //deleteproject will take in id, pass in the id and then return it
 
