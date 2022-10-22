ee8a16c759d7697684b867856a89e3df0bc00221
RomelLopez <romelanthonylopez@gmail.com>
2022-10-22 16:08:57 -0700

    push

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const connectDB = require('./config/db');
-
+const port = process.env.PORT || 5000;
 const cors = require('cors');
 
 const app = express();
@@ -14,7 +14,6 @@ connectDB();
 
 app.use(cors());
 
-
 app.use('/graphql',
     graphqlHTTP({
         schema,
@@ -22,10 +21,10 @@ app.use('/graphql',
     }))
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("public"));
+    app.use(express.static("client/build"));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "public", "index.html"));
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
-const port = process.env.PORT || 5000;
+
 app.listen(port, console.log(`Server running on port ${port}`));
